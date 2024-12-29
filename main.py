839a02224c819b11173181350faf4ea88927aa18
KoreanWienerSchnizel <kwschnizel@gmail.com>
2024-12-28 17:48:29 -0800

    adding player movement

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     print("Starting asteroids!")
@@ -16,10 +21,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill("black")  # fill screen with black
+        for obj in updatable:
+            obj.update(dt)
 
-        player.update(dt)
-        player.draw(screen)
+        screen.fill("black")  # fill screen with black
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
 
